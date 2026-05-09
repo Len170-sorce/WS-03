b339a55f434f66da8e417d1368b8d88de0ee6a79
Len170-sorce <marklennardtorres17@gmail.com>
2026-05-09 18:08:04 +0800

    Remove mysql-8.4 from tracking and add to .gitignore

---
+++ App/views/partials/footer.php
@@ -0,0 +1,23 @@
+<footer style="background-color: #51E2F5;">
+    <div class="container mx-auto px-6 py-6">
+        <div class="flex justify-between items-center">
+            <div>
+                <h3 class="text-2xl font-bold" style="color: #A28089;">Jobseek</h3>
+                <p class="text-sm mt-1" style="color: #A28089;">Find your dream job today.</p>
+            </div>
+            <nav class="flex space-x-6">
+                <a href="/" class="hover:underline font-medium" style="color: #A28089;">Home</a>
+                <a href="/listings" class="hover:underline font-medium" style="color: #A28089;">Jobs</a>
+                <a href="/listings/create" class="hover:underline font-medium" style="color: #A28089;">Post a Job</a>
+            </nav>
+        </div>
+
+        <hr class="my-4" style="border-color: #A28089; opacity: 0.4;">
+
+        <p class="text-center text-sm" style="color: #A28089;">&copy; 2026 Jobseek. All rights reserved.</p>
+    </div>
+</footer>
+
+</body>
+
+</html>
--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once '../helpers.php';
 
-require basePath('Router.php');
+require basePath('Framework/Router.php');
 
-require basePath('Database.php');
+require basePath('Framework/Database.php');
 
 $router = new Router();
 
@@ -12,17 +15,6 @@ $routes = require basePath('routes.php');
 
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$baseUri = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
-
-if ($baseUri !== '' && $baseUri !== '/') {
-    $requestUri = substr($requestUri, strlen($baseUri));
-}
-
-$uri = '/' . trim($requestUri, '/');
-if ($uri === '//') {
-    $uri = '/';
-}
-
 $method = $_SERVER['REQUEST_METHOD'];
 
-$router->route($uri, $method);
+$router->route($requestUri, $method);
