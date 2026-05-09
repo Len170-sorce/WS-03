264019954513027a2d552ab661e8c8ddb194dafd
Len170-sorce <marklennardtorres17@gmail.com>
2026-05-09 19:05:02 +0800

    jobseek part 3 composer

--- public/index.php
+++ public/index.php
@@ -3,16 +3,16 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once '../helpers.php';
 
-require basePath('Framework/Router.php');
+require __DIR__ . '/../vendor/autoload.php';
+require '../helpers.php';
 
-require basePath('Framework/Database.php');
+
+use Framework\Router;
 
 $router = new Router();
 
 $routes = require basePath('routes.php');
-
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
